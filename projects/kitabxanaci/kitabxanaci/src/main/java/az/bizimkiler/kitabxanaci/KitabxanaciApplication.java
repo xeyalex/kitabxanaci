695cf0e37915d71f4a2c28a410d877b42ec761f8
Xeyale Qedirova <xeyaleqediirova@gmail.com>
2024-09-05 01:36:55 +0400

    spring project created

---
+++ projects/kitabxanaci/kitabxanaci/src/main/java/az/bizimkiler/kitabxanaci/KitabxanaciApplication.java
@@ -0,0 +1,13 @@
+package az.bizimkiler.kitabxanaci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitabxanaciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitabxanaciApplication.class, args);
+	}
+
+}
---
+++ projects/kitabxanaci/kitabxanaci/src/test/java/az/bizimkiler/kitabxanaci/KitabxanaciApplicationTests.java
@@ -0,0 +1,13 @@
+package az.bizimkiler.kitabxanaci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitabxanaciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
